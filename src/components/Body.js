f3c416eaa3c966177dd7121de524940dc0a146f4
Aman Kumar Singh <singhaman53146@gmail.com>
2026-01-23 00:22:19 +0530

    Api Calling

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,43 @@
 import RestuarentCard from "./RestuarentCard";
-import resList from "../utils/mockData";    
+//import resList from "../utils/mockData";  
+import { useEffect, useState } from "react";  
 
 
-const Body = () => (
+const Body = () =>{
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+useEffect(()=> {
+
+fetchData();
+
+},[]);
+
+const fetchData = async () =>{
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.63270&lng=77.21980&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+  const json = await data.json();
+  console.log(json);
+  // optional chaining
+  setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants );
+}
+
+  return (
   <div className="body">
-     <div className="search"> Search </div>
+     <div className="filter"> 
+         <button className="filter-btn" onClick={()=>{
+            const filtered = listOfRestaurants.filter((res)=> res.info.avgRating > 4.3);
+            setListOfRestaurants(filtered);
+             console.log(filtered);
+         }}>Top Rated restaurant</button>
+        
+       </div>
      <div className="res-container">
         
-         {resList.map((restaurant) => 
+         {listOfRestaurants.map((restaurant) => 
           (<RestuarentCard key={restaurant.info.id} resData = {restaurant}/>))}
          
      </div>
   </div>
 )
+};
 export default Body; 
